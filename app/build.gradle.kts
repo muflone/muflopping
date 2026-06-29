c040c2ebd6812a23184864cffd3ea94407f0ef78
Muflone <muflone@muflone.com>
2026-06-29 03:07:23 +0200

    Bumped code version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.muflone.muflopping"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "0.1.0"
     }
 
